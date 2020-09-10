3414f549373656cc4e37c3a91043e956d403ceb0
rohits777 <rohitashinde95@gmail.com>
2020-09-11 03:06:28 +0530

    Added: Working Hours is added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,27 +1,31 @@
 echo "Welcome to Employee Wage Computation Program"
 #!/bin/bash
-
 MonthlyWage=0
 WagePerHour=20
 FullTimeHours=8
 HalfTimeHours=4
 WorkingDays=20
-for(( day=1; day<=$WorkingDays; day++ ))
+hours=0
+days=1
+while [[ $hours -le 100 && $days -le 20 ]]
 do
-   randomCheck=`echo $(( RANDOM%3 ))`
-      echo -e "\nDay: $day"
+	randomCheck=`echo $(($RANDOM%3))`
+	echo -e "\nDay: $days"
 	DailyWage=0
-case $randomCheck in
-	"0")echo "Employee is absent" ;;
-	"1")
-	DailyWage=$(( $HalfTimeHours * $WagePerHour ))
-	echo "Employee is present for halfday" 
-	;;
-	"2")
-	DailyWage=$(( $FullTimeHours * $WagePerHour ))
-	echo "Employee is present for full day" 
-	;;
-esac
-   MonthlyWage=$(( $MonthlyWage + $DailyWage ))
+	case $randomCheck in
+		"0")echo "Employee is absent" ;;
+		"1")
+		DailyWage=$(( $HalfTimeHours * $WagePerHour ))
+		hours=$(( $hours + $HalfTimeHours ))
+		echo "Employee is present for halfday" 
+		;;
+		"2")
+		DailyWage=$(( $FullTimeHours * $WagePerHour ))
+		hours=$(( $hours + $FullTimeHours ))
+		echo "Employee is present for full day" 
+		;;
+	esac
+	days=$(( $days + 1 ))
+	MonthlyWage=$(( $MonthlyWage + $DailyWage ))
 	echo -e "DailyWage: $DailyWage     MonthlyWage: $MonthlyWage"
 done
