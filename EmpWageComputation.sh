213491861d4947b814b56c0a8909f10dbbdd4504
rohits777 <rohitashinde95@gmail.com>
2020-09-11 02:34:18 +0530

    Added: Employee Part time Wage added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,13 +4,18 @@ isPresent=1
 DailyWage=0
 WagePerHour=20
 FullTimeHours=8
+HalfTimeHours=4
 randomCheck=$(( RANDOM%2 ))
 
 if [ $isPresent -eq $randomCheck ]
    then
        DailyWage=$(( $WagePerHour * $FullTimeHours )) 
-        echo "Employee is present"
-   else
+        echo "Employee is present full day"
+   elif [[ $RandomCheck -eq $IsPresent ]]
+     then
+	DailyWage=$(( $WagePerHour * $HalfTimeHours ))
+	echo "Employee is present for half day"
+     else
         echo "Employee is Not present"
 fi
 echo "Daily wage of the employee is $DailyWage"
