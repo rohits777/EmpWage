aea77f0388a145d1ea7bb28d2448e1895666319f
rohits777 <rohitashinde95@gmail.com>
2020-09-11 02:42:02 +0530

    Added: Case Statement is added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,21 +1,20 @@
 echo "Welcome to Employee Wage Computation Program"
 #!/bin/bash
-isPresent=1
+
 DailyWage=0
 WagePerHour=20
 FullTimeHours=8
 HalfTimeHours=4
-randomCheck=$(( RANDOM%2 ))
-
-if [ $isPresent -eq $randomCheck ]
-   then
-       DailyWage=$(( $WagePerHour * $FullTimeHours )) 
-        echo "Employee is present full day"
-   elif [[ $RandomCheck -eq $IsPresent ]]
-     then
-	DailyWage=$(( $WagePerHour * $HalfTimeHours ))
-	echo "Employee is present for half day"
-     else
-        echo "Employee is Not present"
-fi
+randomCheck=`echo $(( RANDOM%3 ))`
+case $randomCheck in
+	"0")echo "Employee is absent" ;;
+	"1")
+	DailyWage=$(( $HalfTimeHours * $WagePerHour ))
+	echo "Employee is present for halfday" 
+	;;
+	"2")
+	DailyWage=$(( $FullTimeHours * $WagePerHour ))
+	echo "Employee is present for full day" 
+	;;
+esac
 echo "Daily wage of the employee is $DailyWage"
