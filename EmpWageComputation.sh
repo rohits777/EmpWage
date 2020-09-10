045cb6d3f4f32b61a7d3cf28ae88f0075cee8cea
rohits777 <rohitashinde95@gmail.com>
2020-09-11 03:21:47 +0530

    Refactor: Employee work hours is added using fuction

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -7,25 +7,21 @@ HalfTimeHours=4
 WorkingDays=20
 hours=0
 days=1
-while [[ $hours -le 100 && $days -le 20 ]]
-do
-	randomCheck=`echo $(($RANDOM%3))`
-	echo -e "\nDay: $days"
-	DailyWage=0
-	case $randomCheck in
-		"0")echo "Employee is absent" ;;
-		"1")
-		DailyWage=$(( $HalfTimeHours * $WagePerHour ))
-		hours=$(( $hours + $HalfTimeHours ))
-		echo "Employee is present for halfday" 
-		;;
-		"2")
-		DailyWage=$(( $FullTimeHours * $WagePerHour ))
-		hours=$(( $hours + $FullTimeHours ))
-		echo "Employee is present for full day" 
-		;;
+WorkHours() {
+	local randomCheck=$1
+	case $randomCheck in 
+		"0") echo 0 ;;
+		"1") echo $HalfTimeHours ;;
+		"2") echo $FullTimeHours ;;
 	esac
-	days=$(( $days + 1 ))
+}
+while [[ $days -le 20 && $hours -le 100 ]]
+do
+	randomCheck=`echo $((RANDOM%3))`
+	DayHours="$( WorkHours $randomCheck )"
+	DailyWage=$(( $DayHours * $WagePerHour ))
+	hours=$(( $hours + $DayHours ))
+          echo -e "TotalHours:$hours"
 	MonthlyWage=$(( $MonthlyWage + $DailyWage ))
-	echo -e "DailyWage: $DailyWage     MonthlyWage: $MonthlyWage"
+        days=$(( $days + 1 ))
 done
